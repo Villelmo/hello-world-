9a9db9b38fd75652091e5c811bc2956b3a29b03d
William Torrez Corea <WilliamTorrez@users.noreply.github.com>
2016-04-08 00:55:31 -0600

    Condición

--- valor.c
+++ valor.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int numero;
+  int numero, suma = 0;
   
   printf("Introduce un numero");
-  scanf("%d",&valor);
+  scanf("%d",&numero);
   
+  suma = numero + numero;
+   
+  if (suma == 0) {
+      printf("Esa es la respuesta");
+  }
   return 0; //Programa correcto 
 }
